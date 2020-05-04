74febcdd8b7bddd9494d55c826f7c697c8d26b8a
sahil <s.sahgarg+github@gmail.com>
2020-05-04 19:44:45 +0530

    2020 may number complement, make it fast

--- 2020/05-May/Week1/numberComplement.js
+++ 2020/05-May/Week1/numberComplement.js
@@ -2,11 +2,18 @@
  * @param {number} num
  * @return {number}
  */
+let map = {};
+let getPow = (n) => {
+    if(map[n]) return map[n];
+    map[n] = Math.pow(2, n);
+    return map[n];
+};
+
 var findComplement = function(num) {
     let findMid = (i, j) => {
         if (i === j) return i;
         let mid = Math.floor((i+j)/2);
-        return num < Math.pow(2, mid) ? findMid(i, mid) : findMid(mid+1, j);
+        return num < getPow(mid) ? findMid(i, mid) : findMid(mid+1, j);
     };
-    return num ^ (Math.pow(2, findMid(1, 32)) - 1);
+    return num ^ (getPow(findMid(1, 32)) - 1);
 };
