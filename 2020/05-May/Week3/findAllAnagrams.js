84eed8ecbf17f25a39a60387b8ed22d4daff9347
sahil <s.sahgarg+github@gmail.com>
2020-05-17 15:22:38 +0530

    2020 may: find all anagrams perf

--- 2020/05-May/Week3/findAllAnagrams.js
+++ 2020/05-May/Week3/findAllAnagrams.js
@@ -4,9 +4,9 @@
  * @return {number[]}
  */
 var findAnagrams = function(s, p) {
-    let sLen = s.length, pLen = p.length;
-    if (sLen < pLen) return [];
-    let map = {}, nZero=0, eZero=0;
+    let pLen = p.length;
+    if (s.length < pLen) return [];
+    let map = {}, nZero=0;
     [...p].forEach(ch => {
         if (map[ch] === undefined) map[ch] = 1, nZero++;
         else map[ch]++;
@@ -15,29 +15,29 @@ var findAnagrams = function(s, p) {
         let ch = s[i];
         if (map[ch] !== undefined) {
             map[ch]--;
-            if (map[ch] === 0) eZero++;
-            else if (map[ch] === -1) eZero--;
+            if (map[ch] === 0) nZero--;
+            else if (map[ch] === -1) nZero++;
         }
     }
     let res = [];
-    if (eZero === nZero) res.push(0);
-    for (let i = pLen; i < sLen; i++) {
+    if (nZero === 0) res.push(0);
+    for (let i = pLen; i < s.length; i++) {
         let ch = s[i];
         let rCh = s[i-pLen];
         if (ch !== rCh) {
             if (map[ch] !== undefined) {
                 map[ch]--;
-                if (map[ch] === 0) eZero++;
-                else if (map[ch] === -1) eZero--;
+                if (map[ch] === 0) nZero--;
+                else if (map[ch] === -1) nZero++;
             }
             if (map[rCh] !== undefined) {
                 map[rCh]++;
-                if (map[rCh] === 0) eZero++;
-                else if (map[rCh] === 1) eZero--;
+                if (map[rCh] === 0) nZero--;
+                else if (map[rCh] === 1) nZero++;
             }
         }
 
-        if (eZero === nZero) res.push(i-p.length+1);
+        if (nZero === 0) res.push(i-p.length+1);
     }
     return res;
 };
